f62b940708a660c205716afda7e7a07e501d923f
SamirFarhat17 <samirfarhat17@gmail.com>
2022-01-27 12:23:50 -0600

    Proj 0: Got everything compiling and running plus added working directory

---
+++ Project_0/zonemaps/zonemaps.cpp
@@ -0,0 +1,13 @@
+#include "zonemaps.h"
+
+template<typename T>
+zonemap<T>::zonemap(std::vector<T> _elements, uint32_t _num_elements_per_zone)
+{
+    // constructor
+}
+
+template<typename T>
+bool zonemap<T>::query(T key)
+{
+    return false;
+}
