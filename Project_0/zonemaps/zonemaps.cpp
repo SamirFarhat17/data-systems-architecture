73a33b5bdb635ee7a44bfc5dd1be3846b77f13a6
SamirFarhat17 <samirfarhat17@gmail.com>
2022-01-28 07:40:31 -0600

    Proj 0: Initial query/search work

--- Project_0/zonemaps/zonemaps.cpp
+++ Project_0/zonemaps/zonemaps.cpp
@@ -15,19 +15,31 @@ void zonemap<T>::build()
 {
     std::vector<zone<T>> _zones;
 
+    T temp_min = INT16_MAX;
+    T temp_max = 0;
+    T temp_count = 0;
+
     for(uint32_t i = 0; i  <num_zones; i++) {
         std::vector<T> _temp_zone_elements;
         for(uint32_t j = 0; j < num_elements_per_zone; j++) {
+            temp_count = j;
             if(i*num_elements_per_zone + j < sizeof(elements)){
                 _temp_zone_elements.push_back(elements.at(i*num_elements_per_zone + j));
+                if(elements.at(i*num_elements_per_zone + j)< temp_min) {
+                    temp_min = elements.at(i*num_elements_per_zone + j);
+                }
+                if(elements.at(i*num_elements_per_zone + j) > temp_max) {
+                    temp_max = elements.at(i * num_elements_per_zone + j);
+                }
             }
+            else break;
         }
 
         zone<T> _temp_zone = {
-            _temp_zone_elements,
-            i*num_elements_per_zone,
-            i*num_elements_per_zone + num_elements_per_zone,
-            num_elements_per_zone
+                _temp_zone_elements,
+                temp_min,
+                temp_max,
+                temp_count + 1
         };
 
         _zones.push_back(_temp_zone);
@@ -44,21 +56,28 @@ void zonemap<T>::build()
 template<typename T>
 void zonemap<T>::sort_elements()
 {
-    quicksort(elements, 0, sizeof(elements));
+    for(zone<T> z: zones ) {
+        quicksort(z);
+    }
 }
 
 
 template<typename T>
 bool zonemap<T>::query(T _key)
 {
-
+    quicksort(elements);
+    for(zone<T> z: zones ) {
+        quicksort(z);
+        // search
+    }
+    return false;
 }
 
 
 template<typename T>
 std::vector<T> zonemap<T>::query(T _low, T _high)
 {
-    return false;
+    return zones.at(0);
 }
 
 // Implemented quicksort for sorting elements
@@ -86,4 +105,28 @@ void quicksort(std::vector<T> &_elements, int start, int end ){
 		quicksort(_elements,p+1,end);
 	}
 
+}
+
+// Binary search for key-based queries
+template<typename T>
+bool binary_search(T start, T end, T key) {
+    T lo = start;
+    T up = end;
+
+    while (lo < up)
+    {
+        T mid = lo + (up-lo)/2;
+        if(key == *mid)
+        {
+            return true;
+        }
+        if(key < *mid) {
+            up = mid;
+        }
+        else {
+            lo = mid;
+        }
+    }
+
+    return false;
 }
