9056dc1b6b66cd504f618117ec4fb6bdf1397a33
SamirFarhat17 <samirfarhat17@gmail.com>
2022-01-28 08:05:38 -0600

    Proj 0: Barebones implementation of everything but not really seeing results

--- Project_0/zonemaps/zonemaps.cpp
+++ Project_0/zonemaps/zonemaps.cpp
@@ -6,6 +6,7 @@ zonemap<T>::zonemap(std::vector<T> _elements, uint32_t _num_elements_per_zone)
 {
     // constructor
     elements = _elements;
+    zones = new std::vector<zone<T>>;
     num_elements_per_zone =_num_elements_per_zone;
     num_zones = sizeof(_elements)/_num_elements_per_zone;
 }
@@ -15,7 +16,7 @@ void zonemap<T>::build()
 {
     std::vector<zone<T>> _zones;
 
-    T temp_min = INT16_MAX;
+    T temp_min = INT32_MAX;
     T temp_max = 0;
     T temp_count = 0;
 
@@ -52,7 +53,6 @@ void zonemap<T>::build()
     }
 }
 
-
 template<typename T>
 void zonemap<T>::sort_elements()
 {
@@ -61,23 +61,33 @@ void zonemap<T>::sort_elements()
     }
 }
 
-
 template<typename T>
 bool zonemap<T>::query(T _key)
 {
-    quicksort(elements);
     for(zone<T> z: zones ) {
-        quicksort(z);
-        // search
+        if(z.min >= _key && z.max<= _key) {
+            quicksort(z);
+            if(binary_search(0, sizeof(z), _key)) return true;
+            else continue;
+        }
     }
     return false;
 }
 
-
 template<typename T>
 std::vector<T> zonemap<T>::query(T _low, T _high)
 {
-    return zones.at(0);
+    std::vector<T> found_values;
+    if(_low == _high) return found_values;
+    if(_low == _high) {
+        if(query(_low)) found_values.push_back(_low);
+        return found_values;
+    }
+
+    for(int i = _low; i <= _high; i++) {
+        if(query(i)) found_values.push_back(i);
+    }
+    return found_values;
 }
 
 // Implemented quicksort for sorting elements
@@ -89,7 +99,6 @@ int partition(std::vector<T> &_elements, int start, int end){
 	for(int i=start;i<end;++i){
 		if(_elements[i]<_elements[pivot]){
 			std::swap(_elements[i],_elements[j]);
-			++j;
 		}
 	}
 	std::swap(_elements[j],_elements[pivot]);
@@ -106,7 +115,6 @@ void quicksort(std::vector<T> &_elements, int start, int end ){
 	}
 
 }
-
 // Binary search for key-based queries
 template<typename T>
 bool binary_search(T start, T end, T key) {
