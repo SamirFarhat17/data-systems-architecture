1f0272a2783470bec0085739740fa7b86ce80ad6
SamirFarhat17 <samirfarhat17@gmail.com>
2022-02-04 21:00:16 -0500

    5 million queries still run very very slow

--- Project_0/zonemaps/main.cpp
+++ Project_0/zonemaps/main.cpp
@@ -36,12 +36,17 @@ std::vector<int> generatePointQueries(std::vector<int> data, int n)
 	return queries;
 }
 
-std::vector<tuple<int,int>> generateRangeQueries(std::vector<int> data, int n)
+/**
+     * Purpose: Seperate function to generate the pairs that function as intervals for range queries
+     * Param: the data set
+     * returns: size 4 vector of tuples for the range query batches
+     */
+std::vector<tuple<int,int>> generateRangeQueries(std::vector<int> data)
 {
 	std::vector<tuple<int,int>> range_queries;
 
+	// Get number of elements representing 10% and create pairs accordingly
 	int ten_percent = data.size()/10;
-
 	range_queries.emplace_back(data.at(ten_percent), data.at(ten_percent*2));
 	range_queries.emplace_back(data.at(ten_percent*3), data.at(ten_percent*4));
 	range_queries.emplace_back(data.at(ten_percent*6), data.at(ten_percent*7));
@@ -54,7 +59,7 @@ std::vector<tuple<int,int>> generateRangeQueries(std::vector<int> data, int n)
 int main(int argc, char **argv) {
 
 	if (argc < 3) {
-		std::cout << "Usage: ./main <input_file> <test_case>" << std::endl;
+		// std::cout << "Usage: ./main <input_file> <test_case>" << std::endl;
 		return 0;
 	}
 
@@ -63,7 +68,7 @@ int main(int argc, char **argv) {
 
 	if (test_case != "test_pq" && test_case != "test_rq_1" && test_case 
 	!= "test_rq_2" && test_case != "test_rq_3" && test_case != "test_rq_4") {
-		std::cout
+		 std::cout
 				<< "<test_case> argument has to be one of the following: "
 				<< "test_pq, test_rq_1, test_rq_2, test_rq_3 or test_rq_4"
 				<< std::endl;
@@ -84,20 +89,24 @@ int main(int argc, char **argv) {
 
 	//1. ----------------------------- initialize zonemap and build -----------------------------
 	//build zonemap
-	cout << "zonemap intializing..." << '\n';
-	zonemap<int> zones(data, (uint) data.size() / 10);
-	cout << "zonemap building..." << '\n';
+	//std::cout << "zonemap intializing..." << '\n';
+	// After domains greater than 2 million 10,000 element zones seems to be the sweet spot balance
+	// of quicksort in the build phase and querying in the timed phase
+	zonemap<int> zones(data, (std::min( (uint) data.size() / 100, (uint)10000)));
+	// zonemap building
 	zones.build();
-	cout << "rane queries generating..." << '\n';
-	std::vector<tuple<int,int>> range_queries = generateRangeQueries(data, data.size());
+	// range query generation
+	std::vector<tuple<int,int>> range_queries = generateRangeQueries(data);
 	if (test_case == "test_pq") {
-		cout << "test commencing..." << '\n';
 		//2. ----------------------------- point queries -----------------------------
 		std::vector<int> queries = generatePointQueries(data, data.size());
-		cout << "Point queries generated!" << '\n';
+		//std::cout << "Point queries generated!" << '\n';
 		auto start = std::chrono::high_resolution_clock::now();
+		int count = 0;
+		// Run query for every key in generated queries
 		for (int key: queries) {
-			std::cout << "Querying " << key << "\n" ;
+			/*count++;
+			std::cout << count << '\n';*/
 			zones.query(key);
 		}
 		auto stop = std::chrono::high_resolution_clock::now();
@@ -110,11 +119,9 @@ int main(int argc, char **argv) {
 		//3. ----------------------------- range queries -----------------------------
 		auto start = std::chrono::high_resolution_clock::now();
 		// range query from zonemaps here
+		// get range pair and run
 		tuple<int,int> range_1 = range_queries.at(0);
-		zones.query(std::get<0>(range_1),std::get<1>(range_1));
-		std::cout <<  std::get<0>(range_1) << "," << std::get<1>(range_1) << '\n';
-		int n = data.size();
-
+		std::vector<int> in_range = zones.query(std::get<0>(range_1),std::get<1>(range_1));
 		auto stop = std::chrono::high_resolution_clock::now();
 		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 		unsigned long long range_query_time = duration.count();
@@ -125,10 +132,9 @@ int main(int argc, char **argv) {
 		//3. ----------------------------- range queries -----------------------------
 		auto start = std::chrono::high_resolution_clock::now();
 		// range query from zonemaps here
+		// get range pair and run
 		tuple<int,int> range_2 = range_queries.at(1);
-		zones.query(std::get<0>(range_2),std::get<1>(range_2));
-		int n = data.size();
-
+		std::vector<int> in_range = zones.query(std::get<0>(range_2),std::get<1>(range_2));
 		auto stop = std::chrono::high_resolution_clock::now();
 		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 		unsigned long long range_query_time = duration.count();
@@ -139,10 +145,9 @@ int main(int argc, char **argv) {
 		//3. ----------------------------- range queries -----------------------------
 		auto start = std::chrono::high_resolution_clock::now();
 		// range query from zonemaps here
+		// get range pair and run
 		tuple<int,int> range_3 = range_queries.at(2);
-		zones.query(std::get<0>(range_3),std::get<1>(range_3));
-		int n = data.size();
-
+		std::vector<int> in_range = zones.query(std::get<0>(range_3),std::get<1>(range_3));
 		auto stop = std::chrono::high_resolution_clock::now();
 		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 		unsigned long long range_query_time = duration.count();
@@ -153,10 +158,9 @@ int main(int argc, char **argv) {
 		//3. ----------------------------- range queries -----------------------------
 		auto start = std::chrono::high_resolution_clock::now();
 		// range query from zonemaps here
+		// get range pair and run
 		tuple<int,int> range_4 = range_queries.at(3);
-		zones.query(std::get<0>(range_4),std::get<1>(range_4));
-		int n = data.size();
-
+		std::vector<int> in_range = zones.query(std::get<0>(range_4),std::get<1>(range_4));
 		auto stop = std::chrono::high_resolution_clock::now();
 		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 		unsigned long long range_query_time = duration.count();
@@ -167,3 +171,60 @@ int main(int argc, char **argv) {
 
 	return 0;
 }
+
+/*
+
+Trials: All ran with window threshold at 5%
+
+Running on an HP Spectre x360, 1.8GHZ quad core Intel i7 and 16GB RAM
+* Setup has an extremely limited storage capacity which may skew results
+
+Domain 1 Million | Noise 0% | Seed 1644002859
+point query: 84468882 microseconds
+range query 1: 539 microseconds
+range query 2: 516 microseconds
+range query 3: 511 microseconds
+range query 4: 510 microseconds
+average range query: 519 microseconds
+
+Domain 1 Million | Noise 5% | Seed 1644002865
+point query: 114499908 microseconds
+range query 1: 2733 microseconds
+range query 2: 3057 microseconds
+range query 3: 2805 microseconds
+range query 4: 2533 microseconds
+average range query: 2782 microseconds
+
+Domain 1 Million | Noise 25% | Seed 1644002870
+point query: 166014961 microseconds
+range query 1: 2892 microseconds
+range query 2: 1361 microseconds
+range query 3: 2517 microseconds
+range query 4: 3263 microseconds
+average range query: 2508 microseconds
+
+Domain 5 Million | Noise 0% | Seed 1644002880
+point query:
+range query 1: 1491 microseconds
+range query 2: 1482 microseconds
+range query 3: 1490 microseconds
+range query 4: 1574 microseconds
+average range query: 1509 microseconds
+
+Domain 5 Million | Noise 5% | Seed 1644002884
+point query:
+range query 1: 15091 microseconds
+range query 2: 16124 microseconds
+range query 3: 13618 microseconds
+range query 4: 15070 microseconds
+average range query: 14976
+
+Domain 5 Million | Noise 25% | Seed 1644002889 
+point query:
+range query 1: 18589 microseconds
+range query 2: 12356 microseconds
+range query 3: 12367 microseconds
+range query 4: 5359 microseconds
+average range query: 12176
+
+*/
--- Project_0/zonemaps/zonemaps.cpp
+++ Project_0/zonemaps/zonemaps.cpp
@@ -2,38 +2,41 @@
 #include <assert.h>
 #include <iostream>
 #include <math.h>
-using std::cout;
 
 // Implemented quicksort for sorting elements
 template<typename T>
 T partition(std::vector<T> &vec,  size_t start, size_t end) {
     T pivot = vec.at(start);
-    auto lh = start + 1;
-    auto rh = end;
+    auto l = start + 1;
+    auto r = end;
 
     while (true) {
-        while (lh < rh && vec.at(rh) >= pivot) rh--;
-        while (lh < rh && vec.at(lh) < pivot) lh++;
+        while (l < r && vec.at(r) >= pivot) r--;
+        while (l < r && vec.at(l) < pivot) l++;
+        // found edgecase
+        if (l == r) break;
 
-        if (lh == rh) break;
-
-        T tmp = vec.at(lh);
-        vec.at(lh) = vec.at(rh);
-        vec.at(rh) = tmp;
+        // replace
+        T tmp = vec.at(l);
+        vec.at(l) = vec.at(r);
+        vec.at(r) = tmp;
     }
 
-    if (vec.at(lh) >= pivot) return start;
-    vec.at(start) = vec.at(lh);
-    vec.at(lh) = pivot;
-    return lh;
+    if (vec.at(l) >= pivot) return start;
+    vec.at(start) = vec.at(l);
+    vec.at(l) = pivot;
+    return l;
 }
 template<typename T>
 void sort(std::vector<T> &vec, size_t start, size_t end) {
+    // edgecase done
     if (start >= end)
         return;
 
+    // continue 
     auto boundary = partition(vec, start, end);
 
+    // recursion on separated
     sort(vec, start, boundary);
     sort(vec, boundary + 1, end);
 }
@@ -41,16 +44,14 @@ template<typename T>
 void quicksort(std::vector<T> &vec) {
     sort(vec, 0, vec.size() - 1);
 }
+
 // Binary search for key-based queries
 template<typename T>
 bool binary_search(int start, int end, int key, std::vector<T> _elements) {
 	while (start < end)
 	{
+        // split search interval in halfs and disregard half not in range
 		int mid = (start + end)/2;
-        cout << "key: " << key << '\n';
-        cout << _elements.at(start) << '\n';
-        cout << _elements.at(mid) << '\n';
-        cout << _elements.at(end) << '\n';
 		if(key == _elements.at(mid))
 		{
 			return true;
@@ -65,6 +66,7 @@ bool binary_search(int start, int end, int key, std::vector<T> _elements) {
 	return false;
 }
 
+// Initialize zonemap with basic components
 template<typename T>
 zonemap<T>::zonemap(std::vector<T> _elements, uint _num_elements_per_zone)
 {
@@ -73,16 +75,8 @@ zonemap<T>::zonemap(std::vector<T> _elements, uint _num_elements_per_zone)
     std::vector<zone<T>> _zones;
     zones = _zones;
     num_elements_per_zone =_num_elements_per_zone;
+    // Use ceil to ensure remainder elements have their own zone as well(minimize overhead)
     num_zones = ceil((float)_elements.size()/(float)_num_elements_per_zone);
-    cout << "zonemap initial breakdown:\n";
-    cout << "num elements: " << elements.size() << '\n';
-    cout << "elements: ";
-    for(T element: _elements) {
-        cout << element << " ";
-    }
-    cout << '\n';
-    cout << "num per zone: " << num_elements_per_zone << '\n';
-    cout << "num zones: " << num_zones << "\n\n";
 
 }
 
@@ -91,20 +85,24 @@ void zonemap<T>::build()
 {
     std::vector<zone<T>> _zones;
 
+    // Temporary holders for zone initialization
     T temp_min = INT32_MAX;
     T temp_max = 0;
     T temp_count = 0;
 
     for(uint i = 0; i  < num_zones; i++) {
+        // reset counters
         temp_min = INT32_MAX;
         temp_max = 0;
         std::vector<T> _temp_zone_elements;
         for(uint j = 0; j < num_elements_per_zone; j++) {
             temp_count = j;
+            // So long as zone sizes and elements sizes not broken add to zone
             if(i*num_elements_per_zone + j < elements.size()){
-                _temp_zone_elements.push_back(elements.at(i*num_elements_per_zone + j));
-                if(elements.at(i*num_elements_per_zone + j)< temp_min) {
-                    temp_min = elements.at(i*num_elements_per_zone + j);
+                _temp_zone_elements.push_back(elements.at(i * num_elements_per_zone + j));
+                // Adjust mins and maxes
+                if(elements.at(i * num_elements_per_zone + j) < temp_min) {
+                    temp_min = elements.at(i * num_elements_per_zone + j);
                 }
                 if(elements.at(i*num_elements_per_zone + j) > temp_max) {
                     temp_max = elements.at(i * num_elements_per_zone + j);
@@ -112,7 +110,7 @@ void zonemap<T>::build()
             }
             else break;
         }
-
+        // Initialize zone
         zone<T> _temp_zone = {
                 _temp_zone_elements,
                 temp_min,
@@ -120,79 +118,68 @@ void zonemap<T>::build()
                 temp_count + 1
         };
 
+        // Sort zone prior to prevent overhead while querying(online stage)
+        sort_elements(_temp_zone);
         _zones.push_back(_temp_zone);
     }
 
     zones = _zones;
 
+    // Perform invariant maintenance check which was accounted for earlier 
     for(zone<T> zone: zones ) {
         assert(zone.elements.size() <= num_elements_per_zone);
-        cout << "zone breakdown:\n";
-        cout << "num elements: " << zone.elements.size() << '\n';
-        cout << "elements: ";
-        for(T element: zone.elements) {
-            cout << element << " ";
-        }
-        cout << "\n\n";
     }
 }
 
+// Sort function which just uses zone for simplicity(memory spaceon stack issue though)
 template<typename T>
 void zonemap<T>::sort_elements(zone<T> _zone)
 {
-    cout << "sorting zone from original: ";
-    for(T element: _zone.elements) {
-        cout << element << " ";
-    }
-    cout << '\n';
     quicksort(_zone.elements);
-    cout << "results in: ";
-    for(T element: _zone.elements) {
-        cout << element << " ";
-    }
-    cout << '\n';
 }
 
 template<typename T>
 bool zonemap<T>::query(T _key)
 {
+    // Big O (zone count) for queries(millions occur so not great but not horrible)
     for(zone<T> z: zones ) {
-        cout << "key: " << _key << " zone min: " << z.min << " zone max: " << z.max << '\n';
+        // key is one of the edges of the zone
         if(_key == z.min || _key == z.max) {
-            cout << "found" << '\n';
             return true;
         }
-        if(_key >= z.min && _key <= z.max) {
-            cout << "searching in zone" << '\n';
-            sort_elements(z);
+        // do binary search for efficient location
+        if(_key > z.min && _key < z.max) {
             if(binary_search(0, z.elements.size()-1, _key, z.elements)) {
-                cout << "found" << '\n';
                 return true;
             }
             else continue;
         }
     }
-    cout << "not found" << '\n';
+    // std::cout << "not found" << '\n';
     return false;
 }
 
 template<typename T>
 std::vector<T> zonemap<T>::query(T _low, T _high)
 {
+    // Initialize vector that contains all elements in range
     std::vector<T> found_values;
+    // Edge case for range queries that simplify to single point query
     if(_low == _high) {
         if(query(_low)) found_values.push_back(_low);
         return found_values;
     }
-
+    
     for(zone<T> z: zones) {
+        // Entire zone contains elements in the range(quick)
         if(_low >= z.min && _high <= z.max) {
             found_values.insert(found_values.end(), z.elements.begin(), z.elements.end());
             continue;
         }
+        // Portion of zone contains elements in range
         if((_low >= z.min && _low <= z.max) || (_high <= z.max && _high >= z.min) ) {
             for(T element: z.elements) {
-                if(element >= _low || element <= _high) found_values.push_back(element);
+                if(element >= _low && element <= _high) found_values.push_back(element);
             }
         }
     }
